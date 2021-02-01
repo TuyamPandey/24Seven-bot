8be2202f47ecdee1bbb8985191ec8a6a65ce35b4
TuyamPandey <77833276+TuyamPandey@users.noreply.github.com>
2021-02-01 08:46:31 +0530

    post count edit commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 const { Client, ReactionCollector } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
-const AutoPoster = require('topgg-autoposter')
-const poster = AutoPoster('topggtoken', client);
 const config = require('../slappey.json');
 const client = new Client();
 
 client.config = config;
+
+const AutoPoster = require('topgg-autoposter')
+const ap = AutoPoster("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgwMTEzOTM2NDM3MjQxNDQ3NSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjEyMTQ5MTgzfQ.Zya2K7FM_I4Mzvm-X7JGqM2yr0eXtogokA-9u8y3n2w", client)
 
 const { GiveawaysManager } = require('discord-giveaways')
 client.giveaways = new GiveawaysManager(client, {
